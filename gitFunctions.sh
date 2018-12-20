7534a51dbf8d608a43dc5e54ea06f9d5a5a7bdd7
Drzewiecki Tomasz <tomasz.drzewiecki@nokia.com>
2018-12-20 10:13:41 +0100

    Add git version

---
+++ gitFunctions.sh
@@ -0,0 +1,38 @@
+function createDiffFromRevision()
+{
+  local revision=$1
+  local diffPath=$2
+  git show ${revision} > ${diffPath}
+}
+
+function createInfoFromRevision()
+{
+  local revision=$1
+  local infoPath=$2
+  git show --name-status ${revision} > ${infoFile}
+}
+
+function copyChangedFilesWithHierarchyFromRevision()
+{
+  local revision=$1
+  local outputDir=$2
+  local oldBranchName=$(getBranch)
+  git co ${revision}
+  git diff-tree  --name-status --no-commit-id -r ${revision} | grep -v "^D" | awk -F' ' '{ print $2 }' | xargs -I % cp -r --parents % ${outputDir}/
+  git co ${oldBranchName}
+}
+
+function getRepo()
+{
+  echo `git remote --v | grep origin | grep fetch | awk -F'/' '{print $(NF)}' | awk -F' ' '{print $1}'`
+}
+
+function getBranch()
+{
+  echo `git rev-parse --abbrev-ref HEAD`
+}
+
+function getRepoUrl()
+{
+  echo `git remote --v | grep origin | grep fetch | awk -F' ' '{print $2}'`
+}
